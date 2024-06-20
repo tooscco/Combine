8f9c6a0fb4f9d3c908a875aed29a08e61a42ab93
tooscco <aladejuyigbetosin7@gmail.com>
2024-06-20 12:00:54 +0100

    update

--- Server/index.js
+++ Server/index.js
@@ -1,28 +1,31 @@
-const express =require('express')
-const app = express()
-const dotenv = require("dotenv")
-dotenv.config()
-const mongoose = require ('mongoose')
-const  router  = require("./routes/user.route")
+const express = require('express');
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require('mongoose');
+const router = require("./routes/user.route");
+const cors = require('cors');
+const PORT = process.env.PORT;
+const URI = process.env.URI;
 
-const cors = require('cors')
+app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT
-const URI = process.env.URI
+const corsOptions = {
+  origin: '*',
+  methods: 'GET, POST',
+};
 
-
-app.use(express.urlencoded({extended: true}))
-app.use(cors())
-app.use(express.json())
-app.use("/", router)
-
-
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use("/", router);
 
 app.listen(PORT, () => {
-    mongoose.connect(URI).then(() =>{
-        console.log(('Database connected'));
-    }).catch((err)=>{
-        console.log('Database Could not connect');
-        console.log(err);
+  mongoose.connect(URI)
+    .then(() => {
+      console.log('Database connected');
     })
-})
+    .catch((err) => {
+      console.log('Database could not connect');
+      console.log(err);
+    });
+});
