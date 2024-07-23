12d5204048fbb3e5d52a4874983293e66b7788a3
tooscco <aladejuyigbetosin7@gmail.com>
2024-07-23 18:18:49 +0100

    done

--- Server/index.js
+++ Server/index.js
@@ -5,38 +5,70 @@ dotenv.config();
 const mongoose = require('mongoose');
 const router = require("./routes/user.route");
 const cors = require('cors');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const URI = process.env.URI;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-// Configure CORS
 const corsOptions = {
-  origin: '*',  // Allow all origins
-  methods: 'GET, POST', // Add other methods as needed
-  optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+  origin: '*',
+  methods: 'GET, POST',
 };
 
 app.use(cors(corsOptions));
-
-// Use routes
+app.use(express.json());
 app.use("/", router);
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+app.listen(PORT, () => {
+  mongoose.connect(URI)
+    .then(() => {
+      console.log('Database connected');
+    })
+    .catch((err) => {
+      console.log('Database could not connect');
+      console.log(err);
+    });
 });
 
-// Connect to the database and start the server
-mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Database connected');
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error('Database connection error:', err);
-  });
+// const express = require('express');
+// const app = express();
+// const dotenv = require("dotenv");
+// dotenv.config();
+// const mongoose = require('mongoose');
+// const router = require("./routes/user.route");
+// const cors = require('cors');
+// const PORT = process.env.PORT;
+// const URI = process.env.URI;
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // Configure CORS
+// const corsOptions = {
+//   origin: '*',  // Allow all origins
+//   methods: 'GET, POST', // Add other methods as needed
+//   optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+// };
+
+// app.use(cors(corsOptions));
+
+// // Use routes
+// app.use("/", router);
+
+// // Error handling middleware
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send('Something broke!');
+// });
+
+// // Connect to the database and start the server
+// mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('Database connected');
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error('Database connection error:', err);
+//   });
